00ac9577f0fe9ed12a86b8bbf4b2ec5c6222f136
Ratul Saha <50.ratulsaha@gmail.com>
2015-07-23 19:46:31 +0530

    Update lpc.h

--- lpc.h
+++ lpc.h
@@ -1,13 +1,15 @@
 #ifndef _LPC_H_
 #define _LPC_H_
 
-#define MAX_LPC_ORDER 32
+#define SQRT2 1.41421356237
+#define MAX_LPC_ORDER 60
 
-int compute_lpc_coefs_est(double *autoc, int max_order, double lpc[][MAX_LPC_ORDER]);
-void levinson(double *autoc, int max_order, double *ref,double lpc[][MAX_LPC_ORDER]);
-void acf(double *x, long N, long k, short norm, double *rxx);
-void calc_residue(const double *samples,int N,double *lpc,short order,double *residue);
-void calc_original(const double *residue,int N,double *lpc,short order,double *estimate);
-short GetSignalRA(double *d, long N, double *cof, short P, double *x);
+void acf(double *x,int N,long k,short norm,double *rxx);
+void levinson(double *autoc,unsigned char max_order,double *ref,double lpc[][MAX_LPC_ORDER]);
+unsigned char compute_ref_coefs(double *autoc,unsigned char max_order,double *ref);
+int qtz_ref_cof(double *par,unsigned char ord,short *q_ref);
+int dqtz_ref_cof(const short *q_ref,unsigned char ord,short Q,double *ref);
+void calc_residue(const int *samples,long N,short ord,short Q,int *coff,short *residues);
+void calc_signal(const short *residues,long N,short ord,short Q,int *coff,int *samples);
 
 #endif
