6f5c14b5e3cd878e35c7adcf59d5adad7b757713
Ratul Saha <50.ratulsaha@gmail.com>
2015-07-23 19:49:29 +0530

    Updated Golomb-Rice encoder and decoder

--- rice.c
+++ rice.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
+
 #include "rice.h"
 
 /*signed data to unsigned data*/
@@ -10,7 +11,7 @@ int signed_to_unsigned(const unsigned int data_size,const short *input,unsigned 
 	{
 		if(input[i] < 0)
 			output[i] = (-2 * input[i]) - 1;
-		if(input[i] > 0)
+		if(input[i] >= 0)
 			output[i] = 2 * input[i];
 	}
 	return(0);
@@ -22,7 +23,7 @@ int unsigned_to_signed(const unsigned int data_size,const unsigned short *input,
 	unsigned int i;
 	for(i = 0; i < data_size; i++)
 	{
-		if((input[i] & 0x01) == 0x01)//odd number
+		if((input[i] & 0x01) == 0x01)//Odd number
 			output[i] = -((input[i] + 1) >> 1);
 		if((input[i] & 0x01) == 0x00)//Even number
 			output[i] = input[i] >> 1;
@@ -51,7 +52,7 @@ unsigned short get_opt_rice_param(const unsigned short *data,int data_size,unsig
 		bits[i] = temp;
 	}
 
-	/*Get the minimum parameter*/
+	//Get the minimum parameter
 	temp = bits[0];
 	best_index = 0;
 	*req_bits = bits[0];
@@ -74,17 +75,19 @@ unsigned int rice_encode_block(short param,const unsigned short *input,int size,
 	int i,j,temp,written = 0;
 	unsigned int bits;
 
-	rice_encode_context *ctx = (rice_encode_context *)malloc(sizeof(rice_encode_context));
+	rice_encode_context context;
+	rice_encode_context *ctx = &context;
+
 	ctx->buffer = 0;
 	ctx->filled = 0;
 	ctx->bits = 0;
 
 	for(i = 0; i < size; i++)
 	{
-		/*temp = floor(input[i]/2^param)*/
+		//temp = floor(input[i]/2^param)
 		temp = input[i] >> param;
 
-		/*Write out 'temp' number of ones*/
+		//Write out 'temp' number of ones
 		for(j = 0; j < temp; j++)
 		{
 			ctx->buffer = ctx->buffer | (0x0001 << ctx->filled);
@@ -99,7 +102,7 @@ unsigned int rice_encode_block(short param,const unsigned short *input,int size,
 				ctx->filled++;
 		}
 
-		/*Write out a zero bit*/
+		//Write out a zero bit
 		ctx->buffer = ctx->buffer | (0x0000 << ctx->filled);
 		ctx->bits++;
 		if(ctx->filled == 15)
@@ -111,7 +114,7 @@ unsigned int rice_encode_block(short param,const unsigned short *input,int size,
 		else
 			ctx->filled++;
 
-		/*Write out the last 'param' bits of input[i]*/
+		//Write out the last 'param' bits of input[i]
 		for (j = param - 1; j >= 0; j--)
 		{
 			ctx->buffer = ctx->buffer | (((input[i] >> j) & 0x0001) << ctx->filled);
@@ -127,26 +130,26 @@ unsigned int rice_encode_block(short param,const unsigned short *input,int size,
 		}
 	}
 
-	/*Flush buffer if not empty*/
+	//Flush buffer if not empty
 	if(ctx->filled != 0)
 		encoded[written] = ctx->buffer;
 
 	bits = ctx->bits;
-	free(ctx);
 	return(bits);
 }
 
 /*Decode a block of data encoded using Golomb-Rice coding*/
-unsigned int rice_decode_block(short param,const unsigned short *encoded,int size,unsigned short *decoded)
+unsigned int rice_decode_block(short param,const unsigned short *encoded,int count,unsigned short *decoded)
 {
 	int i = 0,j = 0,q,k = 0;
 	short tmp;
-	rice_decode_context *ctx = (rice_decode_context *)malloc(sizeof(rice_decode_context));
+	rice_decode_context context;
+	rice_decode_context *ctx = &context;
 	ctx->filled = 0;
 
-	while(i < size)
+	while(k < count)
 	{
-		/*Count ones until a zero is encountered*/
+		//Count ones until a zero is encountered
 		q = 0;
 		while(1)
 		{
@@ -162,9 +165,10 @@ unsigned int rice_decode_block(short param,const unsigned short *encoded,int siz
 				break;
 			q++;
 		}
-		decoded[k] = q << param; /*x = q * 2^k*/
+		decoded[k] = 0;
+		decoded[k] = q << param; //x = q * 2^k
 
-		/*Read out the last 'param' bits of input*/
+		//Read out the last 'param' bits of input
 		for(j = param - 1; j >= 0; j--)
 		{
 			if(ctx->filled == 0)
@@ -177,10 +181,9 @@ unsigned int rice_decode_block(short param,const unsigned short *encoded,int siz
 			ctx->filled--;
 			decoded[k] = decoded[k] | (tmp << j);
 		}
-
+		
 		k++;
 	}
 
-	free(ctx);
 	return(k);
 }
