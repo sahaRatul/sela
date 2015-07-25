78f654e5a102efd40d81652412315b7eb8abd9a0
Ratul Saha <50.ratulsaha@gmail.com>
2015-07-25 23:32:42 +0530

    Synced with C code

--- matlab-tests/calc_residue.m
+++ matlab-tests/calc_residue.m
@@ -5,14 +5,24 @@ function residues = calc_residue(samples,lpc_coeffs,Q)
     residues = zeros(1,N);
     
     residues(1) = samples(1);
-    for k = 1:N
+    
+    for k = 2:ord
         y = 2^(Q-1);
-        for i = 1:ord
+        for i = 2:k+1
             y = y + (lpc_coeffs(i) * samples(k-i+1));
-            if(k-i) == 0
+            if((k - i)==0)
                 break;
             end
         end
         residues(k) = samples(k) - y/2^Q;
+    end
+    
+    
+    for k = ord+1:N
+        y = 2^(Q-1);
+        for i = 1:ord
+            y = y + (lpc_coeffs(i) * samples(k-i+1));
+        end
+        residues(k) = y/2^Q;
     end
 end
