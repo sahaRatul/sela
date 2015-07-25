25f62eb65c7063cff69376bd31b5bbb79fd437f1
Ratul Saha <50.ratulsaha@gmail.com>
2015-07-25 23:08:44 +0530

    Update wavwriter.h

--- wavwriter.h
+++ wavwriter.h
@@ -2,24 +2,25 @@
 #define _WAVWRITER_H_
 
 #include <stdio.h>
+
 typedef struct wav_header
 {
 	char riffmarker[4];
-	int filesize;
+	int32_t filesize;
 	char wavemarker[4];
 	char format_tag[4];
-	int format_length;
-	short format_type;
-	short num_channels;
-	int sample_rate;
-	int bytes_per_sec;
-	short bytes_by_capture;
-	short bits_per_sample;
+	int32_t format_length;
+	int16_t format_type;
+	int16_t num_channels;
+	int32_t sample_rate;
+	int32_t bytes_per_sec;
+	int16_t bytes_by_capture;
+	int16_t bits_per_sample;
 	char data_header[4];
-	int data_size;
+	int32_t data_size;
 }wav_header;
 
-void initialize_header(wav_header *hdr,int channels,int rate,int bps);
+void initialize_header(wav_header *hdr,int32_t channels,int32_t rate,int32_t bps);
 void write_header(FILE *fp,wav_header *header);
 void finalize_file(FILE *fp);
 
