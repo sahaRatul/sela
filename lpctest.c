b6ae24ebde0532059e9c4eff2dede1c839f77358
Ratul Saha <50.ratulsaha@gmail.com>
2015-07-25 22:49:57 +0530

    Updated tests

--- lpctest.c
+++ lpctest.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <stdint.h>
 #include <string.h>
 
 #include "lpc.h"
-#include "rice.h"
 
 #define BLOCK_SIZE 2048
 #define SHORT_MAX 32767
 
-int check_wav_file(FILE *fp,int *sample_rate,short *channels,short *bps);
+int32_t check_wav_file(FILE *fp,int32_t *sample_rate,int16_t *channels,int16_t *bps);
 
 int main(int argc,char **argv)
 {
@@ -20,13 +20,13 @@ int main(int argc,char **argv)
 		return -1;
 	}
 
-	int sample_rate;
-	short channels,bps;
-	int corr = 1 << 20;
-	const char Q = 20;
+	int32_t sample_rate;
+	int16_t channels,bps;
+	int32_t corr = 1 << 20;
+	const int8_t Q = 20;
 	
 	check_wav_file(infile,&sample_rate,&channels,&bps);
-	int is_wav = check_wav_file(infile,&sample_rate,&channels,&bps);
+	int32_t is_wav = check_wav_file(infile,&sample_rate,&channels,&bps);
 
 	switch(is_wav)
 	{
@@ -50,65 +50,68 @@ int main(int argc,char **argv)
 	fprintf(stderr,"Bits per sample : %d\n",bps);
 	fprintf(stderr,"Channels : %d\n",channels);
 	fseek(infile,0,SEEK_SET);
-	char header[44];
-	fread(header,sizeof(char),44,infile);
-	fwrite(header,sizeof(char),44,outfile);
+	int8_t header[44];
+	fread(header,sizeof(int8_t),44,infile);
+	fwrite(header,sizeof(int8_t),44,outfile);
 
-	int read_size = BLOCK_SIZE * channels;
-	short *buffer = (short *)malloc(sizeof(short)*read_size);
-	short *rcv_buffer = (short *)malloc(sizeof(short)*read_size);
+	int32_t read_size = BLOCK_SIZE * channels;
+	int16_t *buffer = (int16_t *)malloc(sizeof(int16_t)*read_size);
+	int16_t *rcv_buffer = (int16_t *)malloc(sizeof(int16_t)*read_size);
 
 	while(feof(infile) == 0)
 	{
-		size_t read = fread(buffer,sizeof(short),read_size,infile);
-		int samples_per_channel = read/channels;
+		size_t read = fread(buffer,sizeof(int16_t),read_size,infile);
+		int32_t samples_per_channel = read/channels;
 
-		for(int i = 0; i < channels; i++)
+		for(int32_t i = 0; i < channels; i++)
 		{
-			short samples[BLOCK_SIZE];
-			for(int j = 0; j < samples_per_channel; j++)
+			int16_t samples[BLOCK_SIZE];
+			for(int32_t j = 0; j < samples_per_channel; j++)
 				samples[j] = buffer[channels * j + i];
 
-			int int_samples[BLOCK_SIZE];
-			for(int j = 0; j < samples_per_channel; j++)
+			int32_t int_samples[BLOCK_SIZE];
+			for(int32_t j = 0; j < samples_per_channel; j++)
 				int_samples[j] = samples[j];
 
 			double qtz_samples[BLOCK_SIZE];
-			for(int j = 0; j < samples_per_channel; j++)
+			for(int32_t j = 0; j < samples_per_channel; j++)
 				qtz_samples[j] = ((double)samples[j])/SHORT_MAX;
 
 			double autocorr[MAX_LPC_ORDER + 1];
 			acf(qtz_samples,samples_per_channel,MAX_LPC_ORDER,1,autocorr);
 
 			double ref[MAX_LPC_ORDER];
-			unsigned char opt_lpc_order = compute_ref_coefs(autocorr,MAX_LPC_ORDER,ref);
+			uint8_t opt_lpc_order = compute_ref_coefs(autocorr,MAX_LPC_ORDER,ref);
+
+			//Qtz reflection
+			int32_t q_ref_coeffs[MAX_LPC_ORDER];
+			qtz_ref_cof(ref,opt_lpc_order,q_ref_coeffs);
+
+			//Dequantize
+			dqtz_ref_cof(q_ref_coeffs,opt_lpc_order,Q,ref);
 
 			double lpc[MAX_LPC_ORDER+1];
 			double lpc_mat[MAX_LPC_ORDER][MAX_LPC_ORDER];
 			levinson(NULL,opt_lpc_order,ref,lpc_mat);
 			
-			for(int j = 0; j < opt_lpc_order; j++)
+			for(int32_t j = 0; j < opt_lpc_order; j++)
 				lpc[j+1] = lpc_mat[opt_lpc_order - 1][j];
 			lpc[0] = 0;
 
-			int int_lpc[MAX_LPC_ORDER+1];
-			for(int j = 0; j <= opt_lpc_order; j++)
+			int32_t int_lpc[MAX_LPC_ORDER+1];
+			for(int32_t j = 0; j <= opt_lpc_order; j++)
 				int_lpc[j] =  corr * lpc[j];
 
-			short residues[BLOCK_SIZE];
-			int rcv_samples[BLOCK_SIZE];
+			int32_t residues[BLOCK_SIZE];
+			int32_t rcv_samples[BLOCK_SIZE];
 			calc_residue(int_samples,samples_per_channel,opt_lpc_order,Q,int_lpc,residues);
 
 			calc_signal(residues,samples_per_channel,opt_lpc_order,Q,int_lpc,rcv_samples);
 
-			int short_rcv_samples[BLOCK_SIZE];
-			for(int j = 0; j < samples_per_channel; j++)
-				short_rcv_samples[j] = rcv_samples[j];
-
-			for(int j = 0; j < samples_per_channel; j++)
-				rcv_buffer[channels * j + i] = short_rcv_samples[j];
+			for(int32_t j = 0; j < samples_per_channel; j++)
+				rcv_buffer[channels * j + i] = (int16_t)rcv_samples[j];
 		}
-		fwrite(rcv_buffer,sizeof(short),read,outfile);
+		fwrite(rcv_buffer,sizeof(int16_t),read,outfile);
 	}
 
 
@@ -118,11 +121,11 @@ int main(int argc,char **argv)
 	return 0;
 }
 
-int check_wav_file(FILE *fp,int *sample_rate,short *channels,short *bps)
+int32_t check_wav_file(FILE *fp,int32_t *sample_rate,int16_t *channels,int16_t *bps)
 {
 	fseek(fp,0,SEEK_SET);
-	char header[44];
-	fread(header,sizeof(char),44,fp);
+	int8_t header[44];
+	fread(header,sizeof(int8_t),44,fp);
 
 	/*Check RIFF header*/
 	if(strncmp(header,"RIFF",4)!=0)
@@ -133,13 +136,13 @@ int check_wav_file(FILE *fp,int *sample_rate,short *channels,short *bps)
 		return -2;
 
 	/*Sample Rate*/
-	*sample_rate = *((int *)(header + 24));
+	*sample_rate = *((int32_t *)(header + 24));
 
 	/*Channels*/
-	*channels = *((short *)(header + 22));
+	*channels = *((int16_t *)(header + 22));
 
 	/*Bits per sample*/
-	*bps = *((short *)(header + 34));
+	*bps = *((int16_t *)(header + 34));
 	if(*bps != 16)
 		return -3;
 
