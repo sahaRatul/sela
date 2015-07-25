4b3f5a51f6d398c8334df6e6b8c9a020fe07e9c3
Ratul Saha <50.ratulsaha@gmail.com>
2015-07-25 23:28:03 +0530

    updated with <stdint.h> datatypes

--- format.h
+++ format.h
@@ -1,11 +1,13 @@
 #ifndef _FORMAT_H_
 #define _FORMAT_H_
 
+#include <stdint.h>
+
 typedef struct audio_format
 {
-	int num_channels;
-	int sample_rate;
-	int bits_per_sample;
+	int32_t num_channels;
+	int32_t sample_rate;
+	int32_t bits_per_sample;
 }audio_format;
 
 #endif
