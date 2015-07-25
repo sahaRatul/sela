77b939ddac169dd227f84fcb9cab788e1b2c6456
Ratul Saha <50.ratulsaha@gmail.com>
2015-07-25 22:47:29 +0530

    Update lpc.h

--- lpc.h
+++ lpc.h
@@ -4,12 +4,12 @@
 #define SQRT2 1.41421356237
 #define MAX_LPC_ORDER 60
 
-void acf(double *x,int N,long k,short norm,double *rxx);
-void levinson(double *autoc,unsigned char max_order,double *ref,double lpc[][MAX_LPC_ORDER]);
-unsigned char compute_ref_coefs(double *autoc,unsigned char max_order,double *ref);
-int qtz_ref_cof(double *par,unsigned char ord,short *q_ref);
-int dqtz_ref_cof(const short *q_ref,unsigned char ord,short Q,double *ref);
-void calc_residue(const int *samples,long N,short ord,short Q,int *coff,short *residues);
-void calc_signal(const short *residues,long N,short ord,short Q,int *coff,int *samples);
+void acf(double *x,int32_t N,int64_t k,int16_t norm,double *rxx);
+void levinson(double *autoc,uint8_t max_order,double *ref,double lpc[][MAX_LPC_ORDER]);
+uint8_t compute_ref_coefs(double *autoc,uint8_t max_order,double *ref);
+int32_t qtz_ref_cof(double *par,uint8_t ord,int32_t *q_ref);
+int32_t dqtz_ref_cof(const int32_t *q_ref,uint8_t ord,int16_t Q,double *ref);
+void calc_residue(const int32_t *samples,int64_t N,int16_t ord,int16_t Q,int32_t *coff,int32_t *residues);
+void calc_signal(const int32_t *residues,int64_t N,int16_t ord,int16_t Q,int32_t *coff,int32_t *samples);
 
 #endif
