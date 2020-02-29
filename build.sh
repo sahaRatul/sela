bc18a931bad4d9cee00d12f95cbe2c059233b1c1
Ratul Saha <50.ratulsaha@gmail.com>
2020-02-29 22:38:25 +0530

    Some changes made to build script

--- build.sh
+++ build.sh
@@ -10,3 +10,18 @@ cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . --config Debug -- -j $(nproc)
 # Test
 ctest -j $(nproc) --output-on-failure
+
+cd ..
+
+# Create lcov report
+# capture coverage info
+lcov --directory . --capture --output-file coverage.info
+# filter out system and extra files.
+# To also not include test code in coverage add them with full path to the patterns: '*/tests/*'
+lcov --remove coverage.info "/usr/*" "*.hpp" --output-file coverage.info
+# output coverage data for debugging (optional)
+lcov --list coverage.info
+
+wget https://raw.github.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
+
+python3 lcov_cobertura.py coverage.info --base-dir src/dir --excludes test.lib --output build/coverage.xml
