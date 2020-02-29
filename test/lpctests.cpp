4b1f591a03dd77fd47ba620787c42bbf7575f097
Ratul Saha <50.ratulsaha@gmail.com>
2020-02-29 19:10:18 +0530

    Test case fixes

--- test/lpctests.cpp
+++ test/lpctests.cpp
@@ -3,7 +3,9 @@
 #include "../src/include/lpc.hpp"
 #include "include/catch.hpp"
 
+#ifndef M_PI
 #define M_PI 3.141592653589793238462643383279502884
+#endif
 
 TEST_CASE("LPC Encoder/Decoder combined test")
 {
@@ -12,7 +14,7 @@ TEST_CASE("LPC Encoder/Decoder combined test")
     size_t vecsize = 2048;
     samples.reserve(vecsize);
     for (size_t i = 0; i < vecsize; i++) {
-        samples.push_back((int32_t)(32767 * sin((double)i * (M_PI/180))));
+        samples.push_back((int32_t)(32767 * sin((double)i * (M_PI / 180))));
     }
 
     data::LpcDecodedData* input = new data::LpcDecodedData(samples, (uint8_t)16);
--- test/ricetests.cpp
+++ test/ricetests.cpp
@@ -1,11 +1,17 @@
 #define CATCH_CONFIG_MAIN
 
+#include <random>
+
 #include "../src/include/rice.hpp"
 #include "include/catch.hpp"
 
 TEST_CASE("Rice Encoder/Decoder combined test")
 {
-    data::RiceDecodedData* data = new data::RiceDecodedData(std::vector<int32_t> { 10, -20, 30 });
+    std::vector<int32_t> input = std::vector<int32_t>(2048, 0);
+    for (size_t i = 0; i < input.size(); i++) {
+        input[i] = 200 + (std::rand() % (201));
+    }
+    data::RiceDecodedData* data = new data::RiceDecodedData(input);
     //Encode
     rice::RiceEncoder* enc = new rice::RiceEncoder(*data);
     data::RiceEncodedData encodedData = enc->process();
