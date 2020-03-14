b2d0c731a2359b85f9ac3ec8b65ec026d348bcdc
Ratul Saha <50.ratulsaha@gmail.com>
2020-03-15 00:46:06 +0530

    Doc formatted

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "include/data/exception.hpp"
-#include "include/sela/decoder.hpp"
-#include "include/sela/encoder.hpp"
 #include "include/file/wav_file.hpp"
 #include "include/rice.hpp"
+#include "include/sela/decoder.hpp"
+#include "include/sela/encoder.hpp"
 #include <fstream>
 #include <iostream>
 
-void printUsage(const std::string& programName) {
+void printUsage(const std::string& programName)
+{
     std::string usageString = "";
     usageString += "Usage: \n\n";
     usageString += "Encoding a file:\n";
@@ -16,12 +17,14 @@ void printUsage(const std::string& programName) {
     std::cout << usageString << std::endl;
 }
 
-void encodeFile(std::ifstream& inputFile, std::ofstream& outputFile) {
+void encodeFile(std::ifstream& inputFile, std::ofstream& outputFile)
+{
     sela::Encoder encoder = sela::Encoder(inputFile);
     encoder.process();
 }
 
-void decodeFile(std::ifstream& inputFile, std::ofstream& outputFile) {
+void decodeFile(std::ifstream& inputFile, std::ofstream& outputFile)
+{
     sela::Decoder decoder = sela::Decoder(inputFile);
     decoder.process();
 }
@@ -35,24 +38,21 @@ int main(int argc, char** argv)
     } else {
         try {
             std::string secondArg = std::string(argv[1]);
-            if(secondArg == "-e" && argc == 4) {
+            if (secondArg == "-e" && argc == 4) {
                 std::ifstream inputFile(argv[2], std::ios::binary);
                 std::ofstream outputFile(argv[3], std::ios::binary);
                 encodeFile(inputFile, outputFile);
-            }
-            else if(secondArg == "-d" && argc == 4) {
+            } else if (secondArg == "-d" && argc == 4) {
                 std::ifstream inputFile(argv[2], std::ios::binary);
                 std::ofstream outputFile(argv[3], std::ios::binary);
                 decodeFile(inputFile, outputFile);
-            }
-            else {
+            } else {
                 printUsage(programName);
             }
-        } catch(data::Exception exception) {
+        } catch (data::Exception exception) {
             std::cerr << exception.exceptionMessage << std::endl;
             return 1;
         }
-        
     }
     return 0;
 }
